6020213be0841c774560427ac2d0be76047297a9
Usman Khan <103470619+ukhan55@users.noreply.github.com>
2022-04-12 19:38:37 -0400

    Add files via upload

---
+++ graphicsWindow.py
@@ -0,0 +1,30 @@
+import operator
+from PIL import Image
+
+class graphicsWindow:
+
+    def __init__(self,width=640,height=480):
+        self.__mode = 'RGB'
+        self.__width = width
+        self.__height = height
+        self.__canvas = Image.new(self.__mode,(self.__width,self.__height))
+        self.__image = self.__canvas.load()
+
+    def drawPoint(self,point,color):
+        if 0 <= point[0] < self.__width and 0 <= point[1] < self.__height:
+            self.__image[point[0],point[1]] = color
+
+    def drawLine(self,point1,point2,color):
+
+
+    def saveImage(self,fileName):
+        self.__canvas.save(fileName)
+
+    def showImage(self):
+        self.__canvas.show()
+
+    def getWidth(self):
+        return self.__width
+
+    def getHeight(self):
+        return self.__height
